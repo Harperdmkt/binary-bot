81d4d513541d81988051b75b20fd6a534610c348
Amin Marashi <marashi.s.amin@gmail.com>
2017-05-16 17:16:08 +0800

    Fix proposal test phrases

--- src/botPage/bot/NewTradeEngine/reducers/proposal/index.spec.js
+++ src/botPage/bot/NewTradeEngine/reducers/proposal/index.spec.js
@@ -8,41 +8,41 @@ describe('Proposal Reducer', () => {
     it('Initial state', () => {
         expect((state = proposal(state, action(actions.INVALID)))).toEqual(states.WAITING_FOR_TRADE_OPTION);
     });
-    it('actions.REQUEST_ONE_PROPOSAL request', () => {
+    it('REQUEST_ONE_PROPOSAL request', () => {
         expect((state = proposal(state, action(actions.REQUEST_ONE_PROPOSAL)))).toEqual(
             states.WAITING_FOR_ONE_PROPOSAL
         );
     });
-    it('actions.UPDATE_PROPOSAL go to states.ONE_PROPOSAL_RECEIVED', () => {
+    it('UPDATE_PROPOSAL go to ONE_PROPOSAL_RECEIVED', () => {
         expect(proposal(state, action(actions.UPDATE_PROPOSAL))).toEqual(states.ONE_PROPOSAL_RECEIVED);
     });
-    it('actions.UPDATE_PROPOSAL go to states.ONE_PROPOSAL_RECEIVED', () => {
+    it('UPDATE_PROPOSAL go to ONE_PROPOSAL_RECEIVED', () => {
         expect(proposal(state, action(actions.UPDATE_PROPOSAL))).toEqual(states.ONE_PROPOSAL_RECEIVED);
     });
-    it('actions.REQUEST_TWO_PROPOSALS request', () => {
+    it('REQUEST_TWO_PROPOSALS request', () => {
         expect((state = proposal(states.WAITING_FOR_TRADE_OPTION, action(actions.REQUEST_TWO_PROPOSALS)))).toEqual(
             states.WAITING_FOR_TWO_PROPOSALS
         );
     });
-    it('One actions.UPDATE_PROPOSAL when waiting for two', () => {
+    it('One UPDATE_PROPOSAL when waiting for two', () => {
         expect((state = proposal(state, action(actions.UPDATE_PROPOSAL)))).toEqual(
             states.WAITING_FOR_ONE_MORE_PROPOSAL
         );
     });
-    it('Another actions.UPDATE_PROPOSAL when waiting for one', () => {
+    it('Another UPDATE_PROPOSAL when waiting for one', () => {
         expect((state = proposal(state, action(actions.UPDATE_PROPOSAL)))).toEqual(states.TWO_PROPOSALS_RECEIVED);
     });
-    it('One actions.UPDATE_PROPOSAL when all proposals were received', () => {
+    it('One UPDATE_PROPOSAL when all proposals were received', () => {
         expect((state = proposal(state, action(actions.UPDATE_PROPOSAL)))).toEqual(
             states.WAITING_FOR_ONE_MORE_PROPOSAL
         );
     });
-    it('actions.RENEW_PROPOSALS from states.TWO_PROPOSALS_RECEIVED should go to states.WAITING_FOR_TWO_PROPOSALS', () => {
+    it('RENEW_PROPOSALS from TWO_PROPOSALS_RECEIVED should go to WAITING_FOR_TWO_PROPOSALS', () => {
         expect(proposal(states.TWO_PROPOSALS_RECEIVED, action(actions.RENEW_PROPOSALS))).toEqual(
             states.WAITING_FOR_TWO_PROPOSALS
         );
     });
-    it('actions.RENEW_PROPOSALS from states.ONE_PROPOSAL_RECEIVED should go to states.WAITING_FOR_ONE_PROPOSAL', () => {
+    it('RENEW_PROPOSALS from ONE_PROPOSAL_RECEIVED should go to WAITING_FOR_ONE_PROPOSAL', () => {
         expect(proposal(states.ONE_PROPOSAL_RECEIVED, action(actions.RENEW_PROPOSALS))).toEqual(
             states.WAITING_FOR_ONE_PROPOSAL
         );
