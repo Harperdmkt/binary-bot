3059c98040a5900cca5d909acf6d3ca31903c429
Amin Marashi <marashi.s.amin@gmail.com>
2017-05-16 17:17:59 +0800

    Add data to initializer tests

--- src/botPage/bot/NewTradeEngine/actors/initializer/index.spec.js
+++ src/botPage/bot/NewTradeEngine/actors/initializer/index.spec.js
@@ -7,7 +7,8 @@ describe('Initializer actor', () => {
         const store = createStore();
         const data = { token: 'Xkq6oGFEHh6hJH8', initOptions: { symbol: 'R_100' } };
         await initializer({ data, store });
-        const { stage } = store.getState();
+        const { stage, initData } = store.getState();
         expect(stage).toEqual(states.INITIALIZED);
+        expect(initData).toEqual(data);
     });
 });
