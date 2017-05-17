9702915f5acf8120bdbc60f9803ef7c236b628a4
Amin Marashi <marashi.s.amin@gmail.com>
2017-05-17 16:05:35 +0800

    Complete conditions for watcher

--- src/botPage/bot/NewTradeEngine/actors/watcher/index.js
+++ src/botPage/bot/NewTradeEngine/actors/watcher/index.js
@@ -1,13 +1,16 @@
 import * as states from '../../constants/states';
 import waitForCondition from '../waitForCondition';
 
-const watcher = async ({ store }) => {
+const watcher = async ({ store, name }) => {
     const { stage } = store.getState();
-    if (stage === states.STARTED) {
-        await waitForCondition(store, state => state.stage === states.PROPOSALS_READY);
-        return true;
-    } else if (stage === states.PROPOSALS_READY) {
-        return true;
+    if (name === 'before') {
+        if (stage === states.STARTED) {
+            await waitForCondition(store, state => state.stage === states.PROPOSALS_READY);
+            return true;
+        }
+        return stage === states.PROPOSALS_READY;
+    } else if (name === 'during') {
+        return stage === states.SUCCESSFUL_PURCHASE || stage === states.OPEN_CONTRACT;
     }
     return false;
 };
--- src/botPage/bot/NewTradeEngine/actors/watcher/index.spec.js
+++ src/botPage/bot/NewTradeEngine/actors/watcher/index.spec.js
@@ -3,23 +3,41 @@ import * as actions from '../../constants/actions';
 import watcher from './';
 
 describe('watcher actor', () => {
-    it('should wait for PROPOSALS_READY if STARTED', async () => {
-        const store = createStore();
-        store.dispatch({ type: actions.START, data: {} });
-        setTimeout(() => store.dispatch({ type: actions.RECEIVE_PROPOSALS, data: {} }), 1000);
-        const shouldContinue = await watcher({ store });
-        expect(shouldContinue).toEqual(true);
+    describe('watching before', () => {
+        it('should wait for PROPOSALS_READY if STARTED', async () => {
+            const store = createStore();
+            store.dispatch({ type: actions.START, data: {} });
+            setTimeout(() => store.dispatch({ type: actions.RECEIVE_PROPOSALS, data: {} }), 1000);
+            const shouldContinue = await watcher({ store, name: 'before' });
+            expect(shouldContinue).toEqual(true);
+        });
+        it('should immediately resolve if PROPOSALS_READY', async () => {
+            const store = createStore();
+            store.dispatch({ type: actions.START, data: {} });
+            store.dispatch({ type: actions.RECEIVE_PROPOSALS, data: {} });
+            const shouldContinue = await watcher({ store, name: 'before' });
+            expect(shouldContinue).toEqual(true);
+        });
+        it('should resolve with false if not PROPOSALS_READY or STARTED', async () => {
+            const store = createStore();
+            const shouldContinue = await watcher({ store, name: 'before' });
+            expect(shouldContinue).toEqual(false);
+        });
     });
-    it('should immediately resolve if PROPOSALS_READY', async () => {
-        const store = createStore();
-        store.dispatch({ type: actions.START, data: {} });
-        store.dispatch({ type: actions.RECEIVE_PROPOSALS, data: {} });
-        const shouldContinue = await watcher({ store });
-        expect(shouldContinue).toEqual(true);
-    });
-    it('should resolve with false if not in a correct state', async () => {
-        const store = createStore();
-        const shouldContinue = await watcher({ store });
-        expect(shouldContinue).toEqual(false);
+    describe('watching during', () => {
+        it('should resolve with true if SUCCESSFUL_PURCHASE or OPEN_CONTRACT', async () => {
+            const store = createStore();
+            store.dispatch({ type: actions.PURCHASE_SUCCESSFULLY, data: {} });
+            let shouldContinue = await watcher({ store, name: 'during' });
+            expect(shouldContinue).toEqual(true);
+            store.dispatch({ type: actions.OPEN_CONTRACT_RECEIVED, data: {} });
+            shouldContinue = await watcher({ store, name: 'during' });
+            expect(shouldContinue).toEqual(true);
+        });
+        it('should resolve with false otherwise', async () => {
+            const store = createStore();
+            const shouldContinue = await watcher({ store, name: 'during' });
+            expect(shouldContinue).toEqual(false);
+        });
     });
 });
