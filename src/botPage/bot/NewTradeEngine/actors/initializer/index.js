377d2c0600943d8aac81e1c524edcb0444baf8ef
Amin Marashi <marashi.s.amin@gmail.com>
2017-05-19 11:06:56 +0800

    run initializer only if STOPPED

--- src/botPage/bot/NewTradeEngine/actors/initializer/index.js
+++ src/botPage/bot/NewTradeEngine/actors/initializer/index.js
@@ -1,9 +1,14 @@
 import * as actions from '../../constants/actions';
+import * as states from '../../constants/states';
 import requestTicks from '../../actions/requestTicks';
 import requestBalance from '../../actions/requestBalance';
 import waitForCondition from '../waitForCondition';
 
 const initializer = async ({ data, store }) => {
+    const { stage } = store.getState();
+    if (stage !== states.STOPPED) {
+        return;
+    }
     const { token, initOptions: { symbol } } = data;
     store.dispatch(requestTicks(symbol));
     store.dispatch(requestBalance(token));
